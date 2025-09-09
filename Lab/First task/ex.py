280a4620f7fe8acb6125435ea6f670af7c0bd858
Golubkov Nikita <golubkoffnickita0@gmail.com>
2025-09-09 23:53:45 +0300

    Функция способна обрабатывать отрицательные значения и ноль

--- Lab/First task/ex.py
+++ Lab/First task/ex.py
@@ -5,18 +5,12 @@ def find_target(list_of_numbers: list, target: int):
         a = list_of_numbers[i]
 
         try:
-            if a < target:
-                j = list_of_numbers.index(target - a, i + 1)
-                return [i, j]
+            j = list_of_numbers.index(target - a, i + 1)
+            return [i, j]
         except ValueError:
             pass
         except TypeError:
             if isinstance(target, int):
                 return 'Wrong data type'
             return 'Wrong target type'
-
-
-
-nums = [1, 3, 4]
-target = 4
-print(find_target(nums, target))
+    return None
