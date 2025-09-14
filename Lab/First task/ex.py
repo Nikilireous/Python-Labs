203f676d3a94188ccece455e07a3536875d34a29
Golubkov Nikita <golubkoffnickita0@gmail.com>
2025-09-14 19:49:40 +0300

    Изменена работа функции

--- Lab/First task/ex.py
+++ Lab/First task/ex.py
@@ -1,16 +1,20 @@
 def find_target(list_of_numbers: list, target: int):
+    if not isinstance(list_of_numbers, list):
+        raise TypeError("First variable must be 'list' type")
+    if not isinstance(target, int):
+        raise TypeError("Second variable must be 'int' type")
+
     length = len(list_of_numbers)
 
     for i in range(0, length):
         a = list_of_numbers[i]
+
+        if not isinstance(a, int):
+            raise TypeError("First variable must contain only 'int' objects")
 
         try:
             j = list_of_numbers.index(target - a, i + 1)
             return [i, j]
         except ValueError:
             pass
-        except TypeError:
-            if isinstance(target, int):
-                return 'Wrong data type'
-            return 'Wrong target type'
     return None
